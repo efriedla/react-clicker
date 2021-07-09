24b728ebbc562bba704504dafba85609f6b12810
moocow <efriedla20852@gmail.com>
2021-07-08 23:07:27 -0400

    if tags are empty or has tags function

--- src/components/clicker.jsx
+++ src/components/clicker.jsx
@@ -3,31 +3,22 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        tags: ['tag1','tag2','tag3']
+        tags: ['tag1', 'tag2', 'tag3']
+    }
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>there are no tags </p>;
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
     }
 
     render() {
         return(
             <div>
-                <span className={this.getBadgeClass()}>{this.formateCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-                <ul>
-                    {this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}
-                    
-                </ul>
+
+                {this.renderTags()}
             
             </div>
         );
-    }
-    getBadgeClass() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
-
-    formateCount() {
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
     }
 }
  
