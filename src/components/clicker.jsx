d30d97973a67d67e79eb13c1b15fc0e751ed867e
moocow <efriedla20852@gmail.com>
2021-07-08 23:28:01 -0400

    passing in items

--- src/components/clicker.jsx
+++ src/components/clicker.jsx
@@ -5,8 +5,13 @@ class Counter extends Component {
         count: 0,
     }
 
-    handleIncrement = () => {
+    handleIncrement = (e) => {
+        console.log(e);
         this.setState({ count: this.state.count+ 1});
+    }
+
+    doHandleIncrement = () => {
+        this.handleIncrement({ id: 1 });
     }
 
     render() {
@@ -14,7 +19,7 @@ class Counter extends Component {
             <div>
                 <span className={this.getBadgeClass()}>{this.formateCount()}</span>
                 <button
-                    onClick={this.handleIncrement}
+                    onClick={this.doHandleIncrement}
                     className="btn btn-secondary btn-sm">
                     Increment
                 </button>
