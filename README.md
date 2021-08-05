b737160df3c13c8f5b1a22dccc0764a681f5bc48
moocow <efriedla20852@gmail.com>
2021-08-05 15:38:10 -0400

    rest function added but no real state of truth

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ** https://www.youtube.com/watch?v=Ke90Tje7VS0
 
-props branch use of children and sending props
-list branch uses of classes and props
-child branch the use of multiple components in sync
-delete branch passes the delete function to the child componenet and updating the sate
+* props branch use of children and sending props
+* list branch uses of classes and props
+* child branch the use of multiple components in sync
+* delete branch passes the delete function to the child componenet and updating the sate
+* reset branch passes
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,18 +11,28 @@ class Counters extends Component {
         ]
     }
 
+    handleReset = () => {
+       const counters =  this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({ counters });
+    }
+
     handleDelete = (counterId) => {
         console.log("Delete called ", counterId);
         // create a new counter array and filter out the one being called and let React update the state
         const counters = this.state.counters.filter(c => c.id !== counterId);
-        // this.setState({ counters: counters });
-        //simplified
         this.setState({counters})
     };
 
     render() { 
         return (
             <div>
+                <button
+                    onClick={this.handleReset}
+                    className="btn btn-primary btn-sm m-2"
+                >Reset</button>
                 {this.state.counters.map(counter =>
                     <Counter
                         key={counter.id}
