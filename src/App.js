3da34a9d0824045da947123454d9b4cee4f867c1
moocow <efriedla20852@gmail.com>
2021-08-06 12:12:30 -0400

    added nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Counters from './components/counters';
+import Nav from './components/nav';
 
 function App() {
   console.log("hello!");
   return (
     <div className="App">
+      <Nav />
       <Counters />
       
       
---
+++ src/components/nav.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Nav extends Component{
+    render() {
+        return (
+           <nav className="navbar navbar-light bg-light">
+                <span className="navbar-brand mb-0 h1">Navbar</span>
+            </nav>
+        );
+    }
+
+}
+
+export default Nav;
